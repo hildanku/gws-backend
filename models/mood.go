f7204b361bf1edd2ad8a9f18c1a63f7ec462e3b9
Mzzqq <rezarifa661@gmail.com>
2024-11-25 16:01:07 +0700

    feat: tambahkan model Mood untuk mencatat data mood

---
+++ models/mood.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+type Mood struct {
+	ID           string            `json:"id,omitempty"`
+	UserID       string            `json:"user_id"`
+	Emotion      string            `json:"emotion"`
+	Activities   map[string]string `json:"activities,omitempty"`
+	Note         string            `json:"note,omitempty"`
+	VoiceNoteURL string            `json:"voice_note_url,omitempty"`
+	CreatedAt    time.Time         `json:"created_at,omitempty"`
+}
