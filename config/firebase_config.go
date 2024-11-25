276ad1e8383b0ef5ae2958d4509508c1ea319f0f
Mzzqq <rezarifa661@gmail.com>
2024-11-25 15:05:54 +0700

    feat: tambahkan konfigurasi Firestore client

---
+++ config/firebase_config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"cloud.google.com/go/firestore"
+	"context"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+var FirestoreClient *firestore.Client
+
+func InitFirestore() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	credentials := os.Getenv("FIREBASE_CREDENTIALS")
+	if credentials == "" {
+		log.Fatalf("FIREBASE_CREDENTIALS environment variable not set in .env")
+	}
+
+	client, err := firestore.NewClient(context.Background(), os.Getenv("PROJECT_ID"))
+	if err != nil {
+		log.Fatalf("Error creating Firestore client: %v", err)
+	}
+	FirestoreClient = client
+}
