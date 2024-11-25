32ae087988d7ca614c1455968cb33aba45833966
Mzzqq <rezarifa661@gmail.com>
2024-11-25 16:02:35 +0700

    feat: implementasikan endpoint untuk membuat mood entry
    - Menambahkan fungsi `CreateMoodEntry` di `handlers` untuk menerima data mood dan menyimpannya ke Firestore
    - Menambahkan route `/api/moods` untuk mengakses endpoint `CreateMoodEntry`

---
+++ handlers/mood_handler.go
@@ -0,0 +1,45 @@
+package handlers
+
+import (
+	"context"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gws-app/gws-backend/config"
+	"github.com/gws-app/gws-backend/models"
+	"time"
+)
+
+func CreateMoodEntry(ctx *fiber.Ctx) error {
+	mood := new(models.Mood)
+	if err := ctx.BodyParser(mood); err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(models.MoodResponse{
+			Code:   fiber.StatusBadRequest,
+			Status: "Invalid Input",
+			Data:   nil,
+		})
+	}
+
+	if mood.Emotion == "" {
+		return ctx.Status(fiber.StatusBadRequest).JSON(models.MoodResponse{
+			Code:   fiber.StatusBadRequest,
+			Status: "Emotion is required",
+			Data:   nil,
+		})
+	}
+
+	mood.CreatedAt = time.Now()
+	_, _, err := config.FirestoreClient.Collection("mood_entries").Add(context.Background(), mood)
+	if err != nil {
+		return ctx.Status(fiber.StatusInternalServerError).JSON(models.MoodResponse{
+			Code:   fiber.StatusInternalServerError,
+			Status: "Failed to create mood entry",
+			Data:   nil,
+		})
+	}
+
+	return ctx.Status(fiber.StatusCreated).JSON(models.MoodResponse{
+		Code:   fiber.StatusCreated,
+		Status: "Mood Created",
+		Data:   mood,
+	})
+
+}
---
+++ routes/router.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gws-app/gws-backend/handlers"
+)
+
+func Routes(app *fiber.App) {
+	api := app.Group("/api/moods")
+
+	api.Post("/", handlers.CreateMoodEntry)
+}
